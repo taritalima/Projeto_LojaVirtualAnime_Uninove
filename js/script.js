72f3b22fb359e59d48858794428abfc6dd517254
tarita lima <tarita36@hotmail.com>
2022-05-30 13:23:28 -0300

    site anime

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+
+
+$(document).ready(function () {
+
+    $('#autoWidth').lightSlider({
+
+        autoWidth: true,
+
+        loop: true,
+
+        onSliderLoad: function () {
+
+            $('#autoWidth').removeClass('cS - hidden');
+
+        }
+
+    });
+
+});
+
+
